bfd0606303a1847aae52dac3ec60fe2dd8d09867
dssierra9 <133552168+dssierra9@users.noreply.github.com>
2026-08-13 23:31:55 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,5 @@
 import streamlit as st
+
+st.title("Mi primera app en Streamlit 🚀")
+st.write("¡Hola, Santiago! Tu conexión con GitHub funciona correctamente.")
+st.button("Presiona aquí para probar"
